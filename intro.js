ee1926380cfcd00d58b4a3e19f5684726353857d
Evan Meshberg <emeshberge@gmail.com>
2018-08-20 23:26:08 +0000

    Lesson 4 completed

--- intro.js
+++ intro.js
@@ -4,3 +4,18 @@ $(element).hide().show().css({height: 25});
 
 // Code is less verbose
 // Things get written much quicker
+
+$("#main-heading");
+
+//document.getElementById("page-title");
+$("#page-title");
+
+var heading = $("#page-title");
+
+heading.css({position: "relative"});
+
+heading.animate({left: 100});
+
+heading[0];
+
+
