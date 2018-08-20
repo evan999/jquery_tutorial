03a0a0d36a4fe901aeac7207ad70c5d4ce7e6af3
Evan Meshberg <emeshberge@gmail.com>
2018-08-20 23:31:59 +0000

    Lesson 5 completed

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     alert("jquery loaded");
 })
+
+$("#lead-banner p");
+
+$("h3").css({border: "3px solid blue"});
+
+$(".wrapper").css({border: "3px solid red"});
+
+$("#clients").css({border: "3px solid yellow"});
