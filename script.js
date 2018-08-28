df6298b29e82541e9005ef787a12f6b9456f5f51
Evan Meshberg <emeshberge@gmail.com>
2018-08-28 01:12:16 +0000

    Lesson 12 completed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $("#contact img").removeAttr("alt"); //remove alt attribute completely
 
 $("#contact img").attr("alt", "location"); // set alt attribute to location
 
-console.log($("#contact img").attr("alt")); // read alt attribute
+console.log($("#contact img").attr("alt")); // read alt attribute 
 
 
 
