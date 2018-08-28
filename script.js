077f9024fc3dda3a4ace811218524c0c28bcec55
Evan Meshberg <emeshberge@gmail.com>
2018-08-28 00:16:55 +0000

    Lesson 7 completed

--- script.js
+++ script.js
@@ -48,6 +48,17 @@ $("img[alt]").css({border: "2px solid pink"});
 
 $("img[alt=quote]").css({border: "2px solid purple"});
 
+$("#contact-methods").next().css({border: "3px solid red"});
+
+$("#social-nav").prev().css({border: "3px solid blue"});
+
+$(".banner-title").parents().css({border: "3px solid pink"});
+
+$("#social-nav").children().css({border: "3px solid green"});
+
+$("#contact").find(".facebook").css({border: "3px solid purple"});
+
+$("#social-nav").closest(".wrapper").css({border: "3px solid orange"});
 
 
 
