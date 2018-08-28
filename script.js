c484c421b34209ffe75ab4e38421d6492dbf35a5
Evan Meshberg <emeshberge@gmail.com>
2018-08-28 01:01:46 +0000

    Lesson 11 completed

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-$("section").wrap("<div>");
-
-$("section").unwrap();
-
-$("section").wrapAll("<div>");
-
-var wrapper = "<div class='wrapper'>";
-var button = $(".button");
-var wrapped = true;
-
-button[0].onclick = function(){
-    if(wrapped){
-        $("section").unwrap();
-        wrapped = false;
-        button.text("Wrap");
-    }
-    else{
-        $("section").wrapAll(wrapper);
-        wrapped = true;
-        button.text("Unwrap");
-    }
-};
+$(".button").empty();
+$(".button").remove();
+$("#points-of-sale").empty();
+$("#contact img").remove();
 
 
 
