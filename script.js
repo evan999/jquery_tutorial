b5b099dd90661c82c5da389498538d318e9e4d74
Evan Meshberg <emeshberge@gmail.com>
2018-08-28 03:27:31 +0000

    Lesson 13 completed

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-$("#contact img").removeAttr("alt"); //remove alt attribute completely
+console.log($("#social-nav").css("position"));
+$("#social-nav").css("top", "-200px").css("left", "100px");
 
-$("#contact img").attr("alt", "location"); // set alt attribute to location
+var myObject = {
+    "height": "40",
+    "speed" : "100"
+}
 
-console.log($("#contact img").attr("alt")); // read alt attribute 
+$("#social-nav").css({
+    "top": "-400px",
+    "left" : "150px",
+    "opacity" : "0.5",
+    "border-top" : "4px solid red"
+});
 
 
 
