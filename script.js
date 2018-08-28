ea27d6cb12ec4ed9224940d7d70cb548bd38b353
Evan Meshberg <emeshberge@gmail.com>
2018-08-28 23:32:42 +0000

    Lesson 19 completed

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-// Signal page to load changes before page is fully loaded
-
-$(document).on("ready", function(){
+$(document).ready(function(){
     
-    $("*").on("click", function(){
-        
-       console.log(e.target);
-       console.log("The event type is: " + e.type); 
-       console.log("X coordinate of the event is: " + e.pageX);
-       console.log("Y coordinate of the event is: " + e.pageY);
-       e.stopPropagation();
+    $("section > h2").on("click", function(){
+       
+       $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", complete);
+       
        
     });
-    
-    element.on("click", function(e){
-        
-    });
-    
-});
+})
 
-$(document).ready(function(){
- 
-});
 
-// Wait for element to fully load before changes apply
-
-$(window).on("load", function(){
-    
-});
-
-$(window).onload(function(){
-    
-});
 
 
 
