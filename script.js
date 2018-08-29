1eec3f236ae46a31c66b8770614567d0f49c0f30
Evan Meshberg <emeshberge@gmail.com>
2018-08-29 00:29:11 +0000

    Lesson 20 completed

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
               .fadeTo(200, 0.8)
               .fadeTo(200, 0.2)
               .fadeTo(200, 0.8);
-              
+        
+        
               
        $(this).fadeOut(2000);
        $(this).fadeIn(500);
