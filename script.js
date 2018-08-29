c8197141b4e5a22f8adf9b9dd1cd98caa2f512a6
Evan Meshberg <emeshberge@gmail.com>
2018-08-29 00:12:13 +0000

    Lesson 20 completed

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
     
     $("section > h2").on("click", function(){
        
-       $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", complete);
+       $(this).fadeTo(200, 0.2)
+              .fadeTo(200, 0.8)
+              .fadeTo(200, 0.2)
+              .fadeTo(200, 0.8)
+              .fadeTo(200, 0.2)
+              .fadeTo(200, 0.8);
+              
+              
+       $(this).fadeOut(2000);
+       $(this).fadeIn(500);
+       $(this).animate({ "opacity" : "0.5"});
+       
        
        
     });
