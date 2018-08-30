762be83cf5cc094c2732308f385117595f469749
Evan Meshberg <emeshberge@gmail.com>
2018-08-30 05:21:26 +0000

    Lesson 25 completed

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
+    $(".rslides").responsiveSlides();
+  
     
-    var items = $("#points-of-sale li");
-    
-    items.on("click", function(){
-       
-       $(this).find("p").slideToggle(500);
-        
-    });
 
 });
 
